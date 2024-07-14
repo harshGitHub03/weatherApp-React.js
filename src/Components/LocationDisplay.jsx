0cf45b1723d1ac0156d34d25b01bdc3c0c49101c
harshGit <harshcode07@gmail.com>
2024-07-15 01:25:34 +0530

    first commit

--- src/Components/LocationDisplay.jsx
+++ src/Components/LocationDisplay.jsx
@@ -1,8 +1,8 @@
 const LocationDisplay = ({ status }) => {
     return <>
         <div className="text-white flex flex-col items-end mx-8 my-4 ">
-            <h3 className="text-[5vw] lg:text-6xl font-light">{status.name}</h3>
-            <h5 className="text-3xl font-semibold ">{status.countryCode}</h5>
+            <h3 className="text-[5vw] flex flex-wrap lg:text-7xl font-light">{status.name}</h3>
+            <h5 className="text-4xl font-semibold ">{status.countryCode}</h5>
         </div>
     </>
 }
